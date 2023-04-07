1f7cf0b02962ac9bf2496e3c8864e470fc3b1197
Elvan Hidayat <elvanhd@gmail.com>
2023-04-08 01:52:00 +0700

    creating a sign up page

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -1,6 +1,54 @@
+import { useState } from 'react';
+
 // styles
 import './Signup.css';
 
 export default function Signup() {
-  return <div>Signup</div>;
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [displayName, setDisplayName] = useState('');
+  const [thumbnail, setThumbnail] = useState(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(email, password, displayName);
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className='auth-form'>
+      <h2>sign up</h2>
+      <label>
+        <span>email:</span>
+        <input
+          required
+          type='email'
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+      </label>
+      <label>
+        <span>password:</span>
+        <input
+          required
+          type='password'
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+      </label>
+      <label>
+        <span>display name:</span>
+        <input
+          required
+          type='text'
+          onChange={(e) => setDisplayName(e.target.value)}
+          value={displayName}
+        />
+      </label>
+      <label>
+        <span>Profile thumbnail:</span>
+        <input required type='file' />
+      </label>
+      <button className='btn'>Sign up</button>
+    </form>
+  );
 }
