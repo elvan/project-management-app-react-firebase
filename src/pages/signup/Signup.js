1fb639745f66c1209c50712c05f9a49981a8584d
Elvan Hidayat <elvanhd@gmail.com>
2023-04-08 02:06:05 +0700

    handling file inputs

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -8,10 +8,34 @@ export default function Signup() {
   const [password, setPassword] = useState('');
   const [displayName, setDisplayName] = useState('');
   const [thumbnail, setThumbnail] = useState(null);
+  const [thumbnailError, setThumbnailError] = useState(null);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(email, password, displayName);
+    console.log(email, password, displayName, thumbnail);
+  };
+
+  const handleFileChange = (e) => {
+    setThumbnail(null);
+    let selected = e.target.files[0];
+    console.log(selected);
+
+    if (!selected) {
+      setThumbnailError('Please select a file');
+      return;
+    }
+    if (!selected.type.includes('image')) {
+      setThumbnailError('Selected file must be an image');
+      return;
+    }
+    if (selected.size > 100000) {
+      setThumbnailError('Image file size must be less than 100kb');
+      return;
+    }
+
+    setThumbnailError(null);
+    setThumbnail(selected);
+    console.log('thumbnail updated');
   };
 
   return (
@@ -46,7 +70,8 @@ export default function Signup() {
       </label>
       <label>
         <span>Profile thumbnail:</span>
-        <input required type='file' />
+        <input required type='file' onChange={handleFileChange} />
+        {thumbnailError && <div className='error'>{thumbnailError}</div>}
       </label>
       <button className='btn'>Sign up</button>
     </form>
