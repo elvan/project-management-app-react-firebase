b22b92feb2dbd39c7648543a38613642bcc4fda4
Elvan Hidayat <elvanhd@gmail.com>
2023-04-08 02:09:10 +0700

    firebase storage setup

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,6 +1,7 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/firestore';
+import 'firebase/storage';
 import firebaseConfig from './firebaseConfig';
 
 // init firebase
@@ -9,8 +10,9 @@ firebase.initializeApp(firebaseConfig);
 // init services
 const projectFirestore = firebase.firestore();
 const projectAuth = firebase.auth();
+const projectStorage = firebase.storage();
 
 // timestamp
 const timestamp = firebase.firestore.Timestamp;
 
-export { projectFirestore, projectAuth, timestamp };
+export { projectFirestore, projectAuth, timestamp, projectStorage };
