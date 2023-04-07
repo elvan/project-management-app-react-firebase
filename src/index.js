1eb9cd6c32edc6915804a284814271974430ffc0
Elvan Hidayat <elvanhd@gmail.com>
2023-04-08 01:08:15 +0700

    re-using firebase hooks and auth context

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import { AuthContextProvider } from './context/AuthContext';
+import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <AuthContextProvider>
+      <App />
+    </AuthContextProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
