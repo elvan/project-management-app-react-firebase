cfbb0a13f83945cbcfe2b1e5a95d9df441433552
Elvan Hidayat <elvanhd@gmail.com>
2023-04-09 03:19:02 +0700

    listing comments

--- src/pages/project/ProjectComments.js
+++ src/pages/project/ProjectComments.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import Avatar from '../../components/Avatar';
 import { timestamp } from '../../firebase/config';
 import { useAuthContext } from '../../hooks/useAuthContext';
 import { useFirestore } from '../../hooks/useFirestore';
@@ -30,6 +31,24 @@ export default function ProjectComments({ project }) {
   return (
     <div className='project-comments'>
       <h4>Project Comments</h4>
+
+      <ul>
+        {project.comments.length > 0 &&
+          project.comments.map((comment) => (
+            <li key={comment.id}>
+              <div className='comment-author'>
+                <Avatar src={comment.photoURL} />
+                <p>{comment.displayName}</p>
+              </div>
+              <div className='comment-date'>
+                <p>date here</p>
+              </div>
+              <div className='comment-content'>
+                <p>{comment.content}</p>
+              </div>
+            </li>
+          ))}
+      </ul>
 
       <form className='add-comment' onSubmit={handleSubmit}>
         <label>
